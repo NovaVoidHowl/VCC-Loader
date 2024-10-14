41d35caa9d2d366c642cb4b3a216204fb849624e
NovaVoidHowl <me@novavoidhowl.com>
2024-10-14 16:09:46 +0100

    feat(app): add unity projects section

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const path = require('path');
 const fs = require('fs');
 const storage = require('node-persist');
@@ -19,9 +19,9 @@ async function initializeStorage() {
 initializeStorage();
 
 if (process.env.NODE_ENV === 'development') {
-    require('electron-reload')(__dirname, {
-        electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
-    });
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+  });
 }
 
 let mainWindow;
@@ -31,149 +31,201 @@ let deeplinkingUrl;
 const gotTheLock = app.requestSingleInstanceLock();
 
 if (!gotTheLock) {
-    app.quit();
+  app.quit();
 } else {
-    app.on('second-instance', (event, commandLine, workingDirectory) => {
-        // Someone tried to run a second instance, we should focus our window.
-        if (mainWindow) {
-            if (mainWindow.isMinimized()) mainWindow.restore();
-            mainWindow.focus();
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.focus();
 
-            // Process deeplinking URL from the second instance
-            commandLine.forEach(arg => {
-                if (arg.startsWith('--vcc-url=')) {
-                    deeplinkingUrl = extractDataUrl(arg.split('=')[1]);
-                    console.log(`Parsed deeplinking URL from command-line: ${deeplinkingUrl}`);
-                    mainWindow.webContents.send('deeplinking-url', deeplinkingUrl);
-                } else if (arg.startsWith('vcc://')) {
-                    deeplinkingUrl = extractDataUrl(arg);
-                    console.log(`Parsed deeplinking URL from command-line: ${deeplinkingUrl}`);
-                    mainWindow.webContents.send('deeplinking-url', deeplinkingUrl);
-                }
-            });
-        }
-    });
-
-    // This is the first instance, continue with app initialization
-    app.on('ready', createWindow);
-
-    app.on('window-all-closed', () => {
-        if (process.platform !== 'darwin') {
-            app.quit();
-        }
-    });
-
-    app.on('activate', () => {
-        if (mainWindow === null) {
-            createWindow();
-        }
-    });
-
-    // Parse command-line arguments for the first instance
-    const args = process.argv.slice(1);
-    args.forEach(arg => {
+      // Process deeplinking URL from the second instance
+      commandLine.forEach(arg => {
         if (arg.startsWith('--vcc-url=')) {
-            deeplinkingUrl = extractDataUrl(arg.split('=')[1]);
-            console.log(`Parsed deeplinking URL from command-line: ${deeplinkingUrl}`);
+          deeplinkingUrl = extractDataUrl(arg.split('=')[1]);
+          console.log(`Parsed deeplinking URL from command-line: ${deeplinkingUrl}`);
+          mainWindow.webContents.send('deeplinking-url', deeplinkingUrl);
         } else if (arg.startsWith('vcc://')) {
-            deeplinkingUrl = extractDataUrl(arg);
-            console.log(`Parsed deeplinking URL from command-line: ${deeplinkingUrl}`);
+          deeplinkingUrl = extractDataUrl(arg);
+          console.log(`Parsed deeplinking URL from command-line: ${deeplinkingUrl}`);
+          mainWindow.webContents.send('deeplinking-url', deeplinkingUrl);
         }
+      });
+    }
+  });
+
+  // This is the first instance, continue with app initialization
+  app.on('ready', createWindow);
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+
+  app.on('activate', () => {
+    if (mainWindow === null) {
+      createWindow();
+    }
+  });
+
+  // Parse command-line arguments for the first instance
+  const args = process.argv.slice(1);
+  args.forEach(arg => {
+    if (arg.startsWith('--vcc-url=')) {
+      deeplinkingUrl = extractDataUrl(arg.split('=')[1]);
+      console.log(`Parsed deeplinking URL from command-line: ${deeplinkingUrl}`);
+    } else if (arg.startsWith('vcc://')) {
+      deeplinkingUrl = extractDataUrl(arg);
+      console.log(`Parsed deeplinking URL from command-line: ${deeplinkingUrl}`);
+    }
+  });
+
+  // Handle storing and retrieving URLs
+  ipcMain.on('save-url', async (event, url) => {
+    try {
+      let urls = await storage.getItem('urls') || [];
+      if (!urls.includes(url)) {
+        urls.push(url);
+        await storage.setItem('urls', urls);
+      }
+      event.reply('url-saved', urls);
+    } catch (error) {
+      console.error('Failed to save URL:', error);
+    }
+  });
+
+  ipcMain.on('get-urls', async (event) => {
+    try {
+      const urls = await storage.getItem('urls') || [];
+      event.reply('urls', urls);
+    } catch (error) {
+      console.error('Failed to get URLs:', error);
+    }
+  });
+
+  ipcMain.on('delete-url', async (event, url) => {
+    try {
+      let urls = await storage.getItem('urls') || [];
+      urls = urls.filter(storedUrl => storedUrl !== url);
+      await storage.setItem('urls', urls);
+      event.reply('url-saved', urls);
+    } catch (error) {
+      console.error('Failed to delete URL:', error);
+    }
+  });
+
+  // Handle version number requests
+  ipcMain.on('request-version', (event) => {
+    const packageJsonPath = path.join(__dirname, '..', 'package.json');
+    fs.readFile(packageJsonPath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Failed to read package.json:', err);
+        return;
+      }
+      const packageJson = JSON.parse(data);
+      const appVersion = packageJson.version;
+      event.reply('app-version', appVersion);
+    });
+  });
+
+  // Handle storing and retrieving Unity projects
+  ipcMain.on('save-project', async (event, projectPath) => {
+    try {
+      let projects = await storage.getItem('projects') || [];
+      if (!projects.includes(projectPath)) {
+        projects.push(projectPath);
+        await storage.setItem('projects', projects);
+      }
+      event.reply('project-saved', projects);
+    } catch (error) {
+      console.error('Failed to save project:', error);
+    }
+  });
+
+  ipcMain.on('get-projects', async (event) => {
+    try {
+      const projects = await storage.getItem('projects') || [];
+      event.reply('projects', projects);
+    } catch (error) {
+      console.error('Failed to get projects:', error);
+    }
+  });
+
+  ipcMain.on('delete-project', async (event, projectPath) => {
+    try {
+      let projects = await storage.getItem('projects') || [];
+      projects = projects.filter(storedProject => storedProject !== projectPath);
+      await storage.setItem('projects', projects);
+      event.reply('project-saved', projects);
+    } catch (error) {
+      console.error('Failed to delete project:', error);
+    }
+  });
+
+  ipcMain.on('select-project-folder', async (event) => {
+    const result = await dialog.showOpenDialog(mainWindow, {
+      properties: ['openDirectory']
     });
 
-    // Handle storing and retrieving URLs
-    ipcMain.on('save-url', async (event, url) => {
-        try {
-            let urls = await storage.getItem('urls') || [];
-            if (!urls.includes(url)) {
-                urls.push(url);
-                await storage.setItem('urls', urls);
-            }
-            event.reply('url-saved', urls);
-        } catch (error) {
-            console.error('Failed to save URL:', error);
-        }
-    });
-
-    ipcMain.on('get-urls', async (event) => {
-        try {
-            const urls = await storage.getItem('urls') || [];
-            event.reply('urls', urls);
-        } catch (error) {
-            console.error('Failed to get URLs:', error);
-        }
-    });
-
-    ipcMain.on('delete-url', async (event, url) => {
-        try {
-            let urls = await storage.getItem('urls') || [];
-            urls = urls.filter(storedUrl => storedUrl !== url);
-            await storage.setItem('urls', urls);
-            event.reply('url-saved', urls);
-        } catch (error) {
-            console.error('Failed to delete URL:', error);
-        }
-    });
-
-    // Handle version number requests
-    ipcMain.on('request-version', (event) => {
-        const packageJsonPath = path.join(__dirname, '..', 'package.json');
-        fs.readFile(packageJsonPath, 'utf8', (err, data) => {
-            if (err) {
-                console.error('Failed to read package.json:', err);
-                return;
-            }
-            const packageJson = JSON.parse(data);
-            const appVersion = packageJson.version;
-            event.reply('app-version', appVersion);
-        });
-    });
+    if (!result.canceled && result.filePaths.length > 0) {
+      const projectPath = result.filePaths[0];
+      const isValidUnityProject = fs.existsSync(path.join(projectPath, 'ProjectSettings')) && fs.existsSync(path.join(projectPath, 'Assets'));
+      
+      if (isValidUnityProject) {
+        event.reply('project-saved', [projectPath]);
+        ipcMain.emit('save-project', event, projectPath);
+      } else {
+        dialog.showErrorBox('Invalid Unity Project', 'The selected folder does not appear to be a valid Unity project.');
+      }
+    }
+  });
 }
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        backgroundColor: '#121212', // Set the background color to a dark color
-        show: false, // Don't show the window until it's ready
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-            nodeIntegration: true,
-            contextIsolation: false
-        }
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    backgroundColor: '#121212', // Set the background color to a dark color
+    show: false, // Don't show the window until it's ready
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  mainWindow.loadFile('src/index.html');
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+    if (deeplinkingUrl) {
+      mainWindow.webContents.send('deeplinking-url', deeplinkingUrl);
+    }
+
+    // Read the version number from package.json and send it to the renderer process
+    const packageJsonPath = path.join(__dirname, '..', 'package.json');
+    fs.readFile(packageJsonPath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Failed to read package.json:', err);
+        return;
+      }
+      const packageJson = JSON.parse(data);
+      const appVersion = packageJson.version;
+      mainWindow.webContents.send('app-version', appVersion);
     });
+  });
 
-    mainWindow.loadFile('src/index.html');
-
-    mainWindow.once('ready-to-show', () => {
-        mainWindow.show();
-        if (deeplinkingUrl) {
-            mainWindow.webContents.send('deeplinking-url', deeplinkingUrl);
-        }
-
-        // Read the version number from package.json and send it to the renderer process
-        const packageJsonPath = path.join(__dirname, '..', 'package.json');
-        fs.readFile(packageJsonPath, 'utf8', (err, data) => {
-            if (err) {
-                console.error('Failed to read package.json:', err);
-                return;
-            }
-            const packageJson = JSON.parse(data);
-            const appVersion = packageJson.version;
-            mainWindow.webContents.send('app-version', appVersion);
-        });
-    });
-
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
 }
 
 // Function to extract and decode the actual data URL from the vcc URL
 function extractDataUrl(vccUrl) {
-    const urlMatch = vccUrl.match(/vcc:\/\/vpm\/addRepo\?url=(.*)/);
-    if (urlMatch && urlMatch[1]) {
-        return decodeURIComponent(urlMatch[1]);
-    }
-    return null;
+  const urlMatch = vccUrl.match(/vcc:\/\/vpm\/addRepo\?url=(.*)/);
+  if (urlMatch && urlMatch[1]) {
+    return decodeURIComponent(urlMatch[1]);
+  }
+  return null;
 }
--- src/renderer.js
+++ src/renderer.js
@@ -4,7 +4,11 @@ const semver = require('semver');
 const urlInput = document.getElementById('url');
 const saveUrlButton = document.getElementById('saveUrlButton');
 const storedUrlsList = document.getElementById('storedUrlsList');
+const addProjectButton = document.getElementById('addProjectButton');
+const storedProjectsList = document.getElementById('storedProjectsList');
 const appVersionElement = document.getElementById('app-version');
+const tabs = document.querySelectorAll('.tab');
+const tabContents = document.querySelectorAll('.tab-content');
 
 // Request the version number from the main process on load
 ipcRenderer.send('request-version');
@@ -15,27 +19,35 @@ ipcRenderer.on('app-version', (event, version) => {
 });
 
 ipcRenderer.on('set-url', (event, url) => {
-    console.log(`Received URL in renderer: ${url}`);
-    if (url) {
-        urlInput.value = url;
-        console.log(`URL input box set to: ${url}`);
-    }
+  console.log(`Received URL in renderer: ${url}`);
+  if (url) {
+    urlInput.value = url;
+    console.log(`URL input box set to: ${url}`);
+  }
 });
 
 ipcRenderer.on('deeplinking-url', (event, url) => {
-    console.log(`Received deeplinking URL in renderer: ${url}`);
-    if (urlInput) {
-        urlInput.value = url;
-        console.log(`URL input box set to deeplinking URL: ${url}`);
-    }
+  console.log(`Received deeplinking URL in renderer: ${url}`);
+  if (urlInput) {
+    urlInput.value = url;
+    console.log(`URL input box set to deeplinking URL: ${url}`);
+  }
 });
 
 ipcRenderer.on('url-saved', (event, urls) => {
-    displayStoredUrls(urls);
+  displayStoredUrls(urls);
 });
 
 ipcRenderer.on('urls', (event, urls) => {
-    displayStoredUrls(urls);
+  displayStoredUrls(urls);
+});
+
+ipcRenderer.on('project-saved', (event, projects) => {
+  displayStoredProjects(projects);
+});
+
+ipcRenderer.on('projects', (event, projects) => {
+  displayStoredProjects(projects);
 });
 
 saveUrlButton.addEventListener('click', () => {
@@ -43,11 +55,38 @@ saveUrlButton.addEventListener('click', () => {
   ipcRenderer.send('save-url', url);
 });
 
+addProjectButton.addEventListener('click', () => {
+  ipcRenderer.send('select-project-folder');
+});
+
 storedUrlsList.addEventListener('click', (event) => {
   if (event.target.tagName === 'LI') {
     storedUrlsList.querySelectorAll('li').forEach(li => li.classList.remove('selected'));
     event.target.classList.add('selected');
   }
+});
+
+storedProjectsList.addEventListener('click', (event) => {
+  if (event.target.tagName === 'LI') {
+    storedProjectsList.querySelectorAll('li').forEach(li => li.classList.remove('selected'));
+    event.target.classList.add('selected');
+  }
+});
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    tabs.forEach(t => t.classList.remove('active'));
+    tab.classList.add('active');
+
+    const target = tab.getAttribute('data-tab');
+    tabContents.forEach(content => {
+      if (content.id === target) {
+        content.classList.add('active');
+      } else {
+        content.classList.remove('active');
+      }
+    });
+  });
 });
 
 async function displayStoredUrls(urls) {
@@ -96,5 +135,26 @@ async function displayStoredUrls(urls) {
   }
 }
 
-// Retrieve stored URLs on load
+function displayStoredProjects(projects) {
+  storedProjectsList.innerHTML = '';
+  for (const projectPath of projects) {
+    const li = document.createElement('li');
+    li.classList.add('listing-box');
+    li.innerHTML = `
+      <button class="remove-button">X</button>
+      <div class="info">
+        <h3>${projectPath}</h3>
+      </div>
+    `;
+    li.querySelector('.remove-button').addEventListener('click', () => {
+      if (confirm('Are you sure you want to remove this project?')) {
+        ipcRenderer.send('delete-project', projectPath);
+      }
+    });
+    storedProjectsList.appendChild(li);
+  }
+}
+
+// Retrieve stored URLs and projects on load
 ipcRenderer.send('get-urls');
+ipcRenderer.send('get-projects');
